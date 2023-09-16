6b997cc6be57d6f8f84bad0264fe5cbf24b75c54
zoe <zoeedge0703@gmail.com>
2023-09-16 18:32:17 -0500

    created the putDb

---
+++ client/src/js/database.js
@@ -0,0 +1,28 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jateDB', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jateDB', 1);
+  const text = db.transaction('jate', 'readwrite');
+  const store = text.objectStore('jate');
+  const request = store.put({ value: content });
+  const result = await request;
+  console.log('item added to database', result)
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
